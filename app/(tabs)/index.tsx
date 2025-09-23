1a2414416dd4fce618d2f1003b8e29460aa980fa
Russell Patrick Brown <russellpbrown@gmail.com>
2025-09-23 12:24:27 +0100

    Removed remaining hasOboarded logic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,4 @@
 import OnboardingPage from '@/components/OnboardingPage';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { TabActions, useNavigation } from '@react-navigation/native';
 import { useCallback } from 'react';
 
@@ -7,12 +6,6 @@ export default function HomeScreen() {
   const navigation = useNavigation();
 
   const finishOnboarding = useCallback(async () => {
-    try {
-      await AsyncStorage.setItem(
-        'hasOnboarded',
-        JSON.stringify({ hasOnboarded: true })
-      );
-    } catch {}
     navigation.dispatch(TabActions.jumpTo('meditate'));
   }, [navigation]);
 
