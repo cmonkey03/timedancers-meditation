7a9fd6a57b88be992115c0aa550d0ca54dfd76ad
Russell Patrick Brown <russellpbrown@gmail.com>
2026-07-18 15:07:20 +0200

    Updated explore page links

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -4,9 +4,9 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
 const links = [
-  { label: 'Timedancers.org', url: 'https://timedancers.org' },
-  { label: 'Research: Intimate Violence', url: 'https://timedancers.org/research/feeling-impact' },
-  { label: 'Newsletter', url: 'https://newsletter.timedancers.org' },
+  { label: 'About the creator of this app', url: 'https://russell.dance' },
+  { label: 'Research: Intimate Violence', url: 'https://russell.dance/research/feeling-impact' },
+  { label: 'Newsletter', url: 'https://newsletter.gypsyfires.com' },
 ];
 
 const LinkButton = ({ link }: { link: { label: string; url: string } }) => {
